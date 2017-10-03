07b0eeaa020b6694319258d98433ed695b3fc5f8
ropaha <roga@hagen.me>
2017-10-03 23:06:47 +0200

    smale changes

--- include/client/footer.inc.php
+++ include/client/footer.inc.php
@@ -1,7 +1,7 @@
         </div>
     </div>
     <div id="footer">
-        <p>Copyright &copy; <?php echo date('Y'); ?> <?php echo (string) $ost->company ?: 'osTicket.com'; ?> - All rights reserved.</p>
+        <p>Copyright &copy; <?php echo date('Y'); ?> <?php echo (string) $ost->company ?: 'osTicket.com'; ?>. <?php echo __('All Rights Reserved') ;?>. <?php echo THIS_VERSION ?></p>
         <a id="poweredBy" href="http://osticket.com" target="_blank"><?php echo __('Helpdesk software - powered by osTicket'); ?></a>
     </div>
 <div id="overlay"></div>
