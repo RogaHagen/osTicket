6f22e6d2d3b99dfb2d5198b576cda136c89a77ad
Roga Hagen <roga@hagen.me>
2017-12-01 11:42:01 +0100

    added tooltip

--- include/staff/tickets.inc.php
+++ include/staff/tickets.inc.php
@@ -579,7 +579,7 @@ return false;">
                     ?>
                     <div style="--delta: <?php echo $delta; ?>px; max-height: 1.2em"
                     class="<?php echo ($search && !$status) ? 'tc-title-search' : 'tc-title-main'; ?> link truncate">
-                    <a href="tickets.php?id=<?php echo $T['ticket_id']; ?>"><?php echo $subject; ?></a>
+                    <a title="<?php echo $subject; ?>" href="tickets.php?id=<?php echo $T['ticket_id']; ?>"><?php echo $subject; ?></a>
                     </div>
                     <?php echo Misc::icon_annotation($threadcount, $T['attachment_count'], $T['collab_count'], $task_count, $task_open);?>
                 </td>
